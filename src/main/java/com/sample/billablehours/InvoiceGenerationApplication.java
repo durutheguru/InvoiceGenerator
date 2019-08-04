7192b9145b40068dc6df7de918b8462354aab4a5
Julian Dumebi Duru <dduru@teamapt.com>
2019-08-04 03:02:48 +0100

    changed application name to InvoiceGenerator

--- src/main/java/com/sample/billablehours/InvoiceGenerationApplication.java
+++ src/main/java/com/sample/billablehours/InvoiceGenerationApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BillableHoursApplication {
+public class InvoiceGenerationApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BillableHoursApplication.class, args);
+		SpringApplication.run(InvoiceGenerationApplication.class, args);
 	}
 
 }
